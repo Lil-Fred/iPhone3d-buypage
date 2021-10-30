d0c4979cc753d9c0a00ee3ae1bd08743825b544e
Lil-Fred <leefredrick16@gmail.com>
2021-10-30 10:01:00 +0300

    code cleaner

--- public/index.js
+++ public/index.js
@@ -32,7 +32,7 @@ const navContainer = document.querySelector(".nav-container");
 
 menuIconContainer.addEventListener("click", () => {
     navContainer.classList.toggle("active");
-    document.querySelector('.minus_nav').classList.toggle('hide_behind')
+    document.querySelector('.container').classList.toggle('hide_behind')
 })
 
 
@@ -45,7 +45,7 @@ searchInput.addEventListener("click", () => {
     searchBar.classList.add("active");
     nav.classList.add("move-up");
     desktopNav.classList.add("move-down");
- 
+
 
 })
 
@@ -67,27 +67,26 @@ if (window.innerWidth <= 900) {
     document.getElementById("remove_Br4").remove();
 
 
-    // Removing Network_logos
+    // Removing Carrier_logos
     document.getElementById("remove_ATTlogo").remove();
     document.getElementById("remove_T-mob_Sprintlogo").remove();
     document.getElementById("remove_Verizonlogo").remove();
 
-    // Adding Network_text at the biginning of the H4
-    
+    // Adding Carrier_text at the biginning of the h4
     document
-    .querySelector(".place_a-down")
-    .insertAdjacentElement(
-        "afterend",
-        document.getElementById("displaced_a")
+        .querySelector(".place_a-down")
+        .insertAdjacentElement(
+            "afterend",
+            document.getElementById("displaced_a")
         );
-        document.getElementById("displaced_a").style = "font-size:13.3px;";
-   
-        
-        const para = [
-            document.createElement("span"),
-            document.createElement("span"),
-            document.createElement("span"),
-        ];
+    document.getElementById("displaced_a").style = "font-size:13.3px;";
+
+
+    const para = [
+        document.createElement("span"),
+        document.createElement("span"),
+        document.createElement("span"),
+    ];
     const node = [
         document.createTextNode("AT&T: "),
         document.createTextNode("T-mobile/Sprint: "),
@@ -97,9 +96,9 @@ if (window.innerWidth <= 900) {
     para[1].appendChild(node[1]);
     para[2].appendChild(node[2]);
 
-    para[0].classList.add("network_Txt");
-    para[1].classList.add("network_Txt");
-    para[2].classList.add("network_Txt");
+    para[0].classList.add("Carrier_Txt");
+    para[1].classList.add("Carrier_Txt");
+    para[2].classList.add("Carrier_Txt");
 
     document
         .getElementById("Add_ATT-text")
@@ -111,18 +110,19 @@ if (window.innerWidth <= 900) {
         .getElementById("Add_Verizon-text")
         .insertAdjacentElement("afterbegin", para[2]);
 
+
     const displaced_div = document.getElementById("displaced_div");
     document
         .getElementById("phone_div")
         .insertAdjacentElement("afterbegin", displaced_div);
 
-    const only_mobile = document.getElementById("only_mobile");
+    const iPhone_specialistChat = document.getElementById("iPhone_specialistChat");
     document
         .getElementById("phone_div")
-        .insertAdjacentElement("afterend", only_mobile);
+        .insertAdjacentElement("afterend", iPhone_specialistChat);
 } else {
     //  alert("Resolution is " + window.innerWidth + " X " + window.innerHeight + "." )
-    document.getElementById("only_mobile").remove();
+    document.getElementById("iPhone_specialistChat").remove();
     document.getElementById("remove_Br-desktop").innerText =
         "From $999 or $41.62/mo. before trade-in*";
 }
--- public/main.js
+++ public/main.js
@@ -3,81 +3,65 @@ let scene, camera, renderer, controls, model;
 function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
-  
+
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("phone_div").appendChild(renderer.domElement);
   renderer.domElement.id = "3ddom";
   renderer.domElement.classList.add("off");
 
-   const imgTo3d = document.createElement('span');
-  imgTo3d.classList.add('btnToggle')
-  imgTo3d.innerText ='3D';
-  imgTo3d.addEventListener("click", function () {
-    if (imgTo3d.innerText ==='3D') {
-      imgTo3d.innerText ='✖'
-    } else {
-      imgTo3d.innerText ='3D'
-    }
-    iPhone3D.classList.toggle("off");
-     iphone_img.classList.toggle("off");
-  })
-
-  if (window.innerWidth <= 900) {
-    document.getElementById("displaced_div").insertAdjacentElement('beforeend', imgTo3d);
-    imgTo3d.addEventListener("click", function () {
-      document.querySelector(".buy_sec").style = "padding-right: 0rem;"
-      model.position.set(0, 0, 0); 
-      model.scale.set(2.8, 2.8, 2.8);
-    })
-    
-  }else{
-     document.getElementById("phone_div").insertAdjacentElement('beforebegin', imgTo3d);
-     imgTo3d.addEventListener("click", function () {
-      document.querySelector(".buy_sec_div1").style = "margin-right: 0rem; "
-     document.querySelector(".buy_sec").style = "padding-right: 9rem;"
-    })
-  }
-
-    camera = new THREE.PerspectiveCamera(
-      60,
-      window.innerWidth / window.innerHeight,
-      1,
-      5000
-    );
-    camera.position.set(1, 1, 3);
-    //adjust camera-zoom
-  
-    window.addEventListener("resize", function () {
-      var width = window.innerWidth;
-      var height = window.innerHeight;
-      renderer.setSize(width, height);
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-    });
-
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-
+  //  3D to X SPAN 
+  const imgTomodel = document.createElement('span');
+  imgTomodel.classList.add('spanToggle')
+  imgTomodel.innerText = '3D';
 
   const iphone_img = document.getElementById("iphone-img");
   const iPhone3D = document.getElementById("3ddom");
 
+  imgTomodel.addEventListener("click", function () {
+    if (imgTomodel.innerText === '3D') {
+      imgTomodel.innerText = '✖'
+    } else {
+      imgTomodel.innerText = '3D'
+    }
+    iPhone3D.classList.toggle("off");
+    iphone_img.classList.toggle("off");
+  })
 
+  if (window.innerWidth <= 900) {
+    document.getElementById("displaced_div").insertAdjacentElement('beforeend', imgTomodel);
+    imgTomodel.addEventListener("click", function () {
+      document.querySelector(".buy_sec").style = "padding-right: 0rem;"
+      model.position.set(0, 0, 0);
+      model.scale.set(2.8, 2.8, 2.8);
+    })
 
-  scene.add(new THREE.AxesHelper(500));
+  } else {
+    document.getElementById("phone_div").insertAdjacentElement('beforebegin', imgTomodel);
+    imgTomodel.addEventListener("click", function () {
+      document.querySelector(".buy_sec_div1").style = "margin-right: 0rem; "
+      document.querySelector(".buy_sec").style = "padding-right: 9rem;"
+    })
+  }
 
-  var floorMaterial = new THREE.MeshBasicMaterial({
-    color: 0xc35411,
-    side: THREE.DoubleSide,
+  //CAMERA
+  camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1 , 5000);
+  camera.position.set(1, 1, 3);
+  //adjust camera-zoom
+
+  window.addEventListener("resize", function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
   });
-  var floorGeometry = new THREE.PlaneGeometry(10, 10, 1, 1);
-  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.position.y = -0.5;
-  floor.rotation.x = Math.PI / 2;
-  // Note the mesh is flagged to receive shadows
-  floor.receiveShadow = true;
- // scene.add(floor);
 
+   //CONTROLS
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+
+  //iPhone MODEL
   new THREE.GLTFLoader().load(
     "model/scene.glb",
     (result) => {
@@ -85,15 +69,15 @@ function init() {
       model.scale.set(3.3, 3.3, 3.3);
       model.position.set(.4, 0, 0);
       scene.add(model);
-      
-      animate();
-    }
-    );
-    
-    var ambientLight = new THREE.AmbientLight( 0xffffff, .3 );
-    scene.add( ambientLight );
 
-    let light1 = new THREE.PointLight(0xffffff, 3);
+    }
+  );
+
+  // LIGHTS
+  var ambientLight = new THREE.AmbientLight(0xffffff, .3);
+  scene.add(ambientLight);
+
+  let light1 = new THREE.PointLight(0xffffff, 3);
   light1.position.set(0, 300, 500);
   scene.add(light1);
 
@@ -112,6 +96,8 @@ function init() {
 
   const hemilight = new THREE.HemisphereLight(0x8C9EB4, 0xC0DCF8, 3);
   scene.add(hemilight);
+
+  animate();
 }
 
 
